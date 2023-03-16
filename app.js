c0b49ec36567a380404ec5d9a40c2c124117a6f8
VH Phan <phanveehuen@gmail.com>
2023-03-16 15:43:31 +0800

    Initial commit

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
+app.get('/hello2', function (req, res) {
+    res.send('Hello World2!');
+});
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
