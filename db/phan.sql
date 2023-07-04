a926e7a08e88507af4c4fe99da5defa5b6b8dc04
Phan Vee Huen <phanveehuen@gmail.com>
2023-07-04 12:34:30 +0800

    update from hawk

--- db/phan.sql
+++ db/phan.sql
@@ -3,6 +3,18 @@ select * from hourly_stats.dc_e_nr_events_nrcellcu_flex_raw order by random() li
 select max(date_id) from hourly_stats.dc_e_nr_events_nrcellcu_flex_raw;
 
 
+select * from pg_indexes where tablename like 'clusters_kpi%'
+
+
+
+select * from pg_indexes where schemaname = 'hourly_stats' 
+and
+tablename like 'clusters_kpi%'
+
+
+
+
+
 
 
 
--- db/sandbox3.sql
+++ db/sandbox3.sql
@@ -1,3 +1,49 @@
 SELECT SUM(value)
 FROM unnest(ARRAY[1, 2, 4, null]) AS value;
 
+
+
+
+
+
+
+
+
+create materialized view hourly_stats.clusters_kpi_nr_nrcellcu as
+with dt as (
+    select * from dnb.hourly_stats.dc_e_nr_nrcellcu_raw as t1
+        INNER JOIN dnb.rfdb.cell_mapping as cm on cm."Cellname" = t1."nrcellcu"
+        INNER JOIN dnb.rfdb.df_dpm
+        on cm."SITEID" = df_dpm.site_id
+    WHERE "Region" is not null
+    AND t1."date_id" >= df_dpm.on_board_date::timestamp
+    AND t1."date_id" > now() - interval '14 days'
+)
+select
+date_id,
+"Cluster_ID" as cluster_id,
+100 * sum("endc_sr_nom")|||sum("endc_sr_den")  as  "endc_sr" ,
+100 * sum("erab_drop_call_rate_sgnb_nom")|||sum("erab_drop_call_rate_sgnb_den")  as  "erab_drop_call_rate_sgnb" ,
+100 * sum("intra-sgnb_pscell_change_success_rate_nom")|||sum("intra-sgnb_pscell_change_success_rate_den")  as  "intra-sgnb_pscell_change_success_rate" ,
+100 * sum("inter-sgnb_pscell_change_success_rate_nom")|||sum("inter-sgnb_pscell_change_success_rate_den")  as  "inter-sgnb_pscell_change_success_rate" ,
+100 * sum("rrc_setup_success_rate_signaling_nom")|||sum("rrc_setup_success_rate_signaling_den")  as  "rrc_setup_success_rate_signaling" ,
+100 * sum("endc_ca_configuration_sr_nom")|||sum("endc_ca_configuration_sr_den")  as  "endc_ca_configuration_sr" ,
+100 * sum("endc_ca_deconfiguration_sr_nom")|||sum("endc_ca_deconfiguration_sr_den")  as  "endc_ca_deconfiguration_sr",
+100 * sum("e-rab_block_rate_nom")|||sum("e-rab_block_rate_den")  as  "e-rab_block_rate" 
+from
+dt
+    where "Cluster_ID" is not null
+    GROUP BY "date_id", "Cluster_ID"
+    ;
+
+
+
+
+
+
+
+
+
+
+
+select date_id, count(*) from hourly_stats.dc_e_nr_events_nrcellcu_flex_raw group by date_id order by date_id desc;
